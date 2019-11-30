1dfe8b63a1e1ab05231dd9439684a33f11147821
mvinchankar <mvinchankar18@gmail.com>
2019-11-30 10:48:20 +0530

    ADDED Given the State CSV

--- src/main/java/com/bridgelabz/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -13,10 +14,11 @@ import java.util.Iterator;
 
 
 public class StateCensusAnalyser {
-    private static final String SAMPLE_CSV_FILE_PATH="/home/slot1/StateCode.csv";
+    private static final String SAMPLE_CSV_FILE_PATH="/home/slot1/StateCensusData.csv";
     public static int getCountOfRecords() throws CustomException, IOException {
         int count=0;
         try {
+
             Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
             CsvToBean<CSVStates> csvToBean = new CsvToBeanBuilder(reader)
                     .withIgnoreLeadingWhiteSpace(true)
@@ -29,8 +31,10 @@ public class StateCensusAnalyser {
                 CSVStates csvStates = myUserIterator.next();
                 count++;
             }
-        } catch (NoSuchFileException e){
-            throw new CustomException(CustomException.ExceptionType.NO_SUCH_FILE,"PLease Enter Proper File Path",e);
+        }  catch (NoSuchFileException e) {
+            throw new CustomException(CustomException.ExceptionType.NO_SUCH_FILE, "Please Enter Proper File Path or Type", e);
+        } catch (RuntimeException e){
+            throw new CustomException(CustomException.ExceptionType.INCORRECT_TYPE,"Please Enter type of File",e);
         }
         return count;
     }
--- src/test/java/com/bridgelabz/StateTestCases.java
+++ src/test/java/com/bridgelabz/StateTestCases.java
@@ -8,13 +8,38 @@ import java.io.IOException;
 public class StateTestCases {
 
     @Test
-    public void when_Read_CSV_File_Count_Records_Should_Return_True() throws IOException {
+    public void when_Read_CSV_File_Count_Records_Should_Return_True() {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
         try {
             Assert.assertEquals(37, stateCensusAnalyser.getCountOfRecords());
         } catch (CustomException e) {
-            Assert.assertEquals(CustomException.ExceptionType.NO_SUCH_FILE,e.type);
+            System.out.println(e.getMessage());
+            Assert.assertEquals(CustomException.ExceptionType.NO_SUCH_FILE, e.type);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void when_Incorrect_CSV_File_Should_Return_False() throws IOException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        try {
+            Assert.assertEquals(37, stateCensusAnalyser.getCountOfRecords());
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+            Assert.assertEquals(CustomException.ExceptionType.NO_SUCH_FILE, e.type);
         }
 
+    }
+
+    @Test
+    public void when_InCorrect_CSV_FileType_Should_Return_False() throws IOException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        try {
+            Assert.assertEquals(37, stateCensusAnalyser.getCountOfRecords());
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+            Assert.assertEquals(CustomException.ExceptionType.INCORRECT_TYPE, e.type);
+        }
     }
 }
